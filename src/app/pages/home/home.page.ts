da38deed812f24c2d588638b8cecce03fe86cf4b
Loan Caecke <caecke.loan@gmail.com>
2021-07-09 03:34:30 +0200

    feat(SideMenu/HomePage): Disables the sidemenu on the home/login page

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { MenuController, ToastController } from '@ionic/angular';
 import { IUserCredentials } from 'src/app/interfaces/user-credentials.interface';
 import { AuthService } from 'src/app/services/auth.service';
 
@@ -12,13 +13,36 @@ export class HomePage {
 
   public credentials: IUserCredentials = {} as IUserCredentials;
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    public menuCtrl: MenuController,
+    private toastCtrl: ToastController) { }
 
   public signIn(): void {
-    this.authService.login(this.credentials).then(() => {
+    this.authService.login(this.credentials).then(async () => {
+
+      const toast = await this.toastCtrl.create({
+        message: 'Login successful',
+        duration: 1000,
+        position: 'bottom'
+      });
+      toast.present();
+
+
       // redirect to master list
       this.router.navigate(['/master-list']);
     });
+  }
+
+  ionViewDidEnter(): void {
+    // Disables the side menu for this page.
+    this.menuCtrl.enable(false);
+  }
+
+  ionViewDidLeave(): void {
+    // Re-activate the side menu after login.
+    this.menuCtrl.enable(true);
   }
 
 }
